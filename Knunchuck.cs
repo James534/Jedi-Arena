34cd77b40b90ae652fb55cb9d926b637abc7a7df
James <james534@outlook.com>
2015-11-28 16:23:16 -0500

    Initial Commit
    Initial commit

---
+++ Knunchuck.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using WiimoteApi;
+
+public class Knunchuck : MonoBehaviour {
+
+    Wiimote remote;
+    // Use this for initialization
+    void Start () {
+
+        InitWiimotes();
+        remote.SendPlayerLED(true, false, false, false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    void InitWiimotes()
+    {
+        WiimoteManager.FindWiimotes(); // Poll native bluetooth drivers to find Wiimotes
+        foreach (Wiimote remote in WiimoteManager.Wiimotes)
+        {
+            // Do stuff.
+            Debug.Log("Found Wiimote");
+            this.remote = remote;
+        }
+    }
+}
