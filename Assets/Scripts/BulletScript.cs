289afff98d825596fec0a3847069cdc054030765
Blake Brown <blakebrown129@gmail.com>
2015-11-28 18:55:43 -0500

    Add collision detection for bullets

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+
+	public float speed = 1.0f;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(new Vector3(speed, 0, 0) * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/DeflectBullets.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeflectBullets : MonoBehaviour {
+	
+	BulletScript bulletScript;
+
+	// Use this for initialization
+	void Start () {
+		bulletScript = GameObject.Find("Bullet").GetComponent<BulletScript>();
+		Debug.Log(bulletScript);
+	}
+
+	// If the bullet collides with the lightsaber, deflect the bullet
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("Bullet")) {
+			bulletScript.speed = -1.0f;
+		}
+	}
+}
