91eb487b88c4ee78ff1e0071b972215c5e74ea31
Blake Brown <blakebrown129@gmail.com>
2015-11-28 17:06:56 -0500

    Initial project

---
+++ Assets/Scripts/Lightsaber.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lightsaber : MonoBehaviour {
+	void Update() {
+		if (Input.GetMouseButtonDown (0)) {
+			transform.Rotate(new Vector3(0, 90, 0));
+			
+		}
+		if (Input.GetMouseButtonDown (1)) {
+			Debug.Log("Pressed right click.");
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float speed;
+	public Text countText, winText;
+	private Rigidbody rb;
+	private int count;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		updateScore();
+		winText.text = "";
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce(movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("Pick Up")) {
+			other.gameObject.SetActive(false);
+			count += 1;
+			updateScore();
+		}
+	}
+
+	void updateScore() {
+		countText.text = "Score: " + count.ToString ();
+		if (count >= 10) {
+			winText.text = "You win!";
+		}
+	}
+}
